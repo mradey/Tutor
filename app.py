4a8763e02e97531e7328b05377768cfd06fa5fd8
Michael James Radey <radeymichael@gmail.com>
2019-10-16 16:29:45 -0400

    added tutor response
    tutros can respond for job

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request
 #from twilio.rest import Client
 import pickle
 import smtplib
+import random
+import string
+
+def randomStringDigits(stringLength):
+    """Generate a random string of letters and digits """
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
 
 gmail_user = 'upgradethrowaway@gmail.com'
 gmail_pwd = 'thisisathrowaway'
@@ -13,6 +20,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("findtutor.html")
+@app.route("/activation/<id>")
+def activation(id):
+    return render_template("tutor.html")
 @app.route("/find", methods=['POST'])
 def find():
     #projectpath = request.form['phone']
@@ -22,10 +32,19 @@ def find():
         subject = request.form['sub']
         location = request.form['loc']
         estimated_min = request.form['eta']
-        TEXT = "Hello tutors,\n\nWe need a tutor to help with {} at {} for {} min.  Please click this link and paste this code if interested".format(subject,location,estimated_min)
+        tmpsite = randomStringDigits(10)
+        TEXT = "Hello tutors,\n\nWe need a tutor to help with {} at {} for {} min.  Please click the link below and paste this code if interested: {}\n\n http://127.0.0.1:5000/activation/{}".format(subject,location,estimated_min,tmpsite,tmpsite)
+        #get all tutor phones
         with open('tutors.pickle', 'rb') as handle:
             d = pickle.load(handle)
             TO = [d[tutorPhone]["email"] for tutorPhone in d]
+
+        #add tutoring opportunity
+        with open('current_opportunities.pickle', 'rb') as handle:
+            d = pickle.load(handle)
+        d[tmpsite] = {"tutorfound":False, "phone":phone, "subject":subject, "location":location,"estimated_min":estimated_min}
+        with open('current_opportunities.pickle', 'wb') as handle:
+            pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
         message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
         """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
         try:
@@ -35,9 +54,25 @@ def find():
             server.login(gmail_user, gmail_pwd)
             server.sendmail(FROM, TO, message)
             server.close()
-            return "Tutors have been alerted! We will text you back once someone has responded. <a href='./'>return</a>"
+            return "Tutors have been alerted! We will text you back once someone has responded. <a href='./'>return</a>".format(tmpsite)
         except:
             return "Could not send mail, please try again: <a href='./'>return</a>"
+@app.route("/proposal", methods=['POST'])
+def proposal():
+    if request.method == "POST":
+        code = request.form['code']
+        with open('current_opportunities.pickle', 'rb') as handle:
+            d = pickle.load(handle)
+        print(d[code]["tutorfound"])
+        print(d[code]["tutorfound"] == True)
+        if d[code]["tutorfound"]:
+            return "Sorry a tutor was already found for this position. We will alert you for future opportunities."
+        else:
+            d[code]["tutorfound"] = True
+            with open('current_opportunities.pickle', 'wb') as handle:
+                pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            return str(d[code])
+        return code
 @app.route("/register", methods=['POST'])
 def register():
     #projectpath = request.form['phone']
@@ -47,6 +82,7 @@ def register():
         phone = request.form['phone']
         email = request.form['email']
         major = request.form['major']
+        #add tutor
         with open('tutors.pickle', 'rb') as handle:
             unserialized_data = pickle.load(handle)
         unserialized_data[phone] = {"name":name,"email":email,"major":major}
--- tmp.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-with open('tutors.pickle', 'rb') as handle:
-    d = pickle.load(handle)
-print([d[tutorPhone]["email"] for tutorPhone in d])
